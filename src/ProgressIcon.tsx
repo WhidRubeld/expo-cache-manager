e65526c3c229d11c3f345babca6328ef91f964ce
Gennady Vasilev <whidrubeld@gmail.com>
2023-02-10 00:21:28 +0300

    update typing

--- src/ProgressIcon.tsx
+++ src/ProgressIcon.tsx
@@ -2,17 +2,19 @@ import type { ColorValue } from 'react-native'
 import { CacheEntryStatus } from './CacheEntry.class'
 import { DownloadIcon, PauseIcon, PlayIcon } from './icons'
 
+export type ProgressIconProps = {
+  status: CacheEntryStatus
+  size: number
+  color?: ColorValue
+  toggleButtons?: boolean
+}
+
 export const ProgressIcon = ({
   status,
   size,
   color = '#ffffff',
   toggleButtons = false
-}: {
-  status: CacheEntryStatus
-  size: number
-  color?: ColorValue
-  toggleButtons?: boolean
-}) => {
+}: ProgressIconProps) => {
   switch (status) {
     case CacheEntryStatus.Pending: {
       return <DownloadIcon width={size} height={size} fill={color} />

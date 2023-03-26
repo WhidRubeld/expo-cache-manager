4f920afa80949161fc31543bc384de561c3deee7
Gennady Vasilev <whidrubeld@gmail.com>
2023-03-26 17:49:27 +0300

    Remove unused code and simplify rendering in App component

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,13 +1,10 @@
 import { CacheManagerProvider } from 'expo-cache-manager'
-import { useState } from 'react'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
 import { StatusBar } from 'expo-status-bar'
 
 import Page from './Page'
 
 export default function App() {
-  const [ready, setReady] = useState(false)
-
   return (
     <>
       <StatusBar style='dark' />
@@ -21,9 +18,8 @@ export default function App() {
             { name: 'videos' },
             'other'
           ]}
-          onReady={() => setReady(true)}
         >
-          {ready && <Page />}
+          <Page />
         </CacheManagerProvider>
       </SafeAreaProvider>
     </>
